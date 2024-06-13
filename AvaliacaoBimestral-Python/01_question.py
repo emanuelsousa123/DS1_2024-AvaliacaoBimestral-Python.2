02bfa3ee7e11ef5508f8fac7781d19e728d85d00
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-06-13 05:49:56 -0300

    Questão 1 modificada

--- AvaliacaoBimestral-Python/01_question.py
+++ AvaliacaoBimestral-Python/01_question.py
@@ -1,6 +1,4 @@
 nome = input("Digite seu nome: ")
-nome = nome.capitalize()
 sobrenome = input("Digite seu sobrenome: ")
-sobrenome = sobrenome.capitalize()
-nome_completo = nome + " " + sobrenome
+nome_completo = nome.capitalize() + " " + sobrenome.capitalize()
 print(f"Seu nome completo é {nome_completo}.")
