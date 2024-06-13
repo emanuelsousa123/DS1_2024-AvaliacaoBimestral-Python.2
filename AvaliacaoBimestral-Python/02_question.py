56d4a3a3fb02622c2adfaffa6429811f679616c2
emanuelsousa123 <168196582+emanuelsousa123@users.noreply.github.com>
2024-06-13 06:44:34 -0300

    Questão 2 modificada

--- AvaliacaoBimestral-Python/02_question.py
+++ AvaliacaoBimestral-Python/02_question.py
@@ -2,10 +2,10 @@ def agilidade(var):
     print(f'Essa variável contém "{var}" e seu tipo é {type(var)}.\n')
  
 ano = 1999
-agilidade(ano)
 nota = 0.83
-agilidade(nota)
 filme = "Matrix"
-agilidade(filme)
 red_pill = True
-agilidade(red_pill)
+
+vars = [ano, nota, filme, red_pill]
+for x in vars:
+    agilidade(x)
